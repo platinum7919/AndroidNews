f3dafa7dbc645809be87cb0aa82848f25eba763d
Pan <ng.yat.pan@gmail.com>
2019-11-18 14:51:40 +0800

    testests

--- app/src/main/java/com/androidnews/App.kt
+++ app/src/main/java/com/androidnews/App.kt
@@ -26,7 +26,7 @@ class App : Application(), HasActivityInjector {
     override fun onCreate() {
         super.onCreate()
         //app = this
-        //test again master
+        //test again 1111
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
